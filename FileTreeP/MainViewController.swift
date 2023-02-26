5c43791dbab7dacc68691c096f48be219deae662
Vladimir Lukyanenko <>
2023-02-26 14:48:21 +0200

    preparation for work

--- FileTreeP/MainViewController.swift
+++ FileTreeP/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  FileTreeP
 //
 //  Created by Vladimir Lukyanenko on 26.02.2023.
@@ -7,11 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
