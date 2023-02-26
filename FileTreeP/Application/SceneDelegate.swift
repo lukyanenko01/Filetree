401427591e20dc3881c6feccd7a3b0f5baf6eb50
Vladimir Lukyanenko <>
2023-02-26 15:15:14 +0200

    setupNavigationBar

--- FileTreeP/Application/SceneDelegate.swift
+++ FileTreeP/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         let vc = MainViewController()
-        window.rootViewController = vc
+        window.rootViewController = UINavigationController(rootViewController: vc)
         window.makeKeyAndVisible()
         self.window = window
     }
--- FileTreeP/MainViewController.swift
+++ FileTreeP/MainViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class MainViewController: UIViewController {
+    
+  
+   
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        setupNavigationBar()
     }
 
 
+    private func setupNavigationBar() {
+        let addFileButton = UIBarButtonItem(image: UIImage(systemName: "doc.fill.badge.plus"), style: .plain, target: self, action: #selector(addFileButtonAction))
+        let addFolderButton = UIBarButtonItem(image: UIImage(systemName: "folder.fill.badge.plus"), style: .plain, target: self, action: #selector(addFolderButtonAction))
+        let switchLayoutButton = UIBarButtonItem(image: UIImage(systemName: "list.bullet"), style: .plain, target: self, action: #selector(switchLayoutButtonAction))
+        
+        let profileButton = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle.fill"), style: .plain, target: self, action: #selector(profileButtonAction))
+        
+        navigationController?.modalTransitionStyle = .coverVertical
+        navigationItem.title = "File Tree"
+        navigationController?.view.backgroundColor = .clear
+        navigationItem.rightBarButtonItems = [switchLayoutButton, addFolderButton, addFileButton]
+        navigationItem.leftBarButtonItem = profileButton
+        
+    }
+    
+    @objc private func addFileButtonAction() {
+        
+    }
+    
+    @objc private func addFolderButtonAction() {
+        
+    }
+    
+    @objc private func switchLayoutButtonAction() {
+        
+    }
+    
+    @objc private func profileButtonAction() {
+        
+    }
 }
 
